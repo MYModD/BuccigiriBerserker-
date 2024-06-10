9d36b993a3dfca59fa4e3617714d89e79eedabc3
yuru58 <169129939+yuru58@users.noreply.github.com>
2024-06-10 17:01:56 +0900

    敵の出現

--- Assets/Member/Nakahira/Script/Set  On.cs
+++ Assets/Member/Nakahira/Script/Set  On.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SetOn : MonoBehaviour
 {
     //public Transform plyerp;
+  
     public Transform teki;
     public float tekikyuri;
     // Start is called before the first frame update
@@ -16,15 +17,17 @@ public class SetOn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject child = transform.GetChild(0).gameObject;
         Vector3 targetPos = teki.position;
-        float distance = Vector3.Distance(transform.position, teki.position);
+        
         Setonp();
     }
     public void Setonp()
     {
-        //if(distance = tekikyuri)
-        //{
-
-        //}
+        float distance = Vector3.Distance(transform.position, teki.position); 
+        if (distance == tekikyuri)
+        {
+            
+        }
     }
 }
