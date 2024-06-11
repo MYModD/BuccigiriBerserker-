9e6eb3a0b0de1a978f8f0e1e7828c7282cde57e0
yuru58 <169129939+yuru58@users.noreply.github.com>
2024-06-11 15:02:32 +0900

    敵出現

--- Assets/Member/Nakahira/Script/Set  On.cs
+++ Assets/Member/Nakahira/Script/Set  On.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class SetOn : MonoBehaviour
 {
-    //public Transform plyerp;
-  
+    
+    public BoxCollider tekihako;
+    public MeshRenderer tekimmes;
     public Transform teki;
     public float tekikyuri;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tekimmes.enabled = false;
+        tekihako.enabled = false;
+        GameObject.FindWithTag("teki");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject child = transform.GetChild(0).gameObject;
+      ///*  GameObject child = transform.GetChild(0).gameObjec*/t;
         Vector3 targetPos = teki.position;
         
         Setonp();
     }
     public void Setonp()
     {
+        //Transform target3 = teki.transform.Find("tekiko");
         float distance = Vector3.Distance(transform.position, teki.position); 
-        if (distance == tekikyuri)
+        if (distance < tekikyuri)
         {
-            
+            tekihako.enabled = true;
+            tekimmes.enabled = true;
         }
     }
 }
