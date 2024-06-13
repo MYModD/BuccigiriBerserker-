9ea967db80e0e69bbe101ee3f6054a911aeba940
yuru58 <169129939+yuru58@users.noreply.github.com>
2024-06-13 11:53:50 +0900

    敵の出現と削除終わり

--- Assets/Member/Nakahira/Script/Misail_Hit.cs
+++ Assets/Member/Nakahira/Script/Misail_Hit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Misail_Hit : MonoBehaviour
 {
     //public Transform missile;
-    public Transform player;
+    //public Transform player;
     // Start is called before the first frame update
 
     void Start()
@@ -16,7 +16,7 @@ public class Misail_Hit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = player.position;
+        //Vector3 targetPos = player.position;
     }
     private void OnTriggerEnter(Collider coll)
     {
--- Assets/Member/Nakahira/Script/Set_On.cs
+++ Assets/Member/Nakahira/Script/Set_On.cs
@@ -4,39 +4,70 @@ using UnityEngine;
 
 public class Set_On : MonoBehaviour
 {
-    public BoxCollider tekihako;
-    public MeshRenderer tekimmes;
-    public Transform teki;
-    public float tekikyuri;
+    // ゲームオブジェクトのボックスコライダーとメッシュレンダラー
+    public BoxCollider myhako;
+    public MeshRenderer mymmes;
+    // プレイヤーの位置とプレイヤーとの距離
+    public Transform plyer;
+    public float plykyori;
+    // ターゲットの位置
+    public Transform target;
+    // 移動速度
+    public float moveSpeed;
+
+    public float stop;
+    public float mo;
+    public float st;
+    public Transform target2;
+    public Transform thisobj;
     // Start is called before the first frame update
     void Start()
     {
-        tekimmes.enabled = false;
-        tekihako.enabled = false;
-        GameObject[] tekif = GameObject.FindGameObjectsWithTag("teki");
-        foreach (GameObject obj in tekif)
-        {
-            Collider collider = obj.GetComponent<Collider>();
-            MeshRenderer meshRenderer = obj.GetComponent<MeshRenderer>();
-        }
+        // メッシュレンダラーを非表示にする
+        mymmes.enabled = false;
+        myhako.enabled = false;
+        //GameObject[] tekif = GameObject.FindGameObjectsWithTag("teki");
+        //foreach (GameObject obj in tekif)
+        //{
+        //    Collider collider = obj.GetComponent<Collider>();
+        //    MeshRenderer meshRenderer = obj.GetComponent<MeshRenderer>();
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 targetPos = teki.position;
+        Vector3 targetPos = plyer.position;
 
         Setonp();
     }
     public void Setonp()
     {
         //Transform target3 = teki.transform.Find("tekiko");
-        float distance = Vector3.Distance(transform.position, teki.position);
-        if (distance < tekikyuri)
+        float distance = Vector3.Distance(transform.position, plyer.position);
+        if (distance < plykyori)
         {
-            tekihako.enabled = true;
-            tekimmes.enabled = true;
+            //敵のしゅつげん
+            myhako.enabled = true;
+            mymmes.enabled = true;
+
+            Vector3 targetPos = target.position;
+            transform.LookAt(targetPos);
+            Vector3 targetPos2 = target2.position;
+            float distancepl = Vector3.Distance(transform.position, target.position);// このオブジェクトとターゲットの距離を計算
+
+            // ターゲットに向かって移動
+            if (distancepl > stop)
+            {
+                transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+            }
+            else if (distancepl < stop)
+            {
+                // ターゲット2に向かって移動
+                transform.LookAt(targetPos2);
+                transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
+            }
         }
     }
 }
