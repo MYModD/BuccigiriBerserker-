3b2115ee10b3f09345dd49e6240ca829b577fe67
MorikuboP <orevshokuto10@gmail.com>
2024-06-14 12:31:23 +0900

    回転を作ってる途中だよ
    ハッカのにおい嫌いじゃない

--- Assets/Member/Hama-/Script/Roll_Move.cs
+++ Assets/Member/Hama-/Script/Roll_Move.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class Roll_Move : MonoBehaviour
 {
-    private float speed = 10f;
+    private float _speed = 10f;
+
+    private float _rolling = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+
+
+
+
+        if (Input.GetButtonDown("Lift_Roll"))
+        {
+
+            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            this.transform.Rotate(0, 0, 360 * _rolling);
+
+        }
+
+        if (Input.GetButtonDown("Right_Roll"))
+        {
+
+            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            this.transform.Rotate(0, 0, 360 * _rolling);
+
+        }
+
     }
 }
