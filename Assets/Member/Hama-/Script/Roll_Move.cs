3f170a8f8f372c9e31b79f1690ff602122017597
MorikuboP <orevshokuto10@gmail.com>
2024-06-16 18:36:45 +0900

    回避完成
    動くと回転がでかくなる

--- Assets/Member/Hama-/Script/Roll_Move.cs
+++ Assets/Member/Hama-/Script/Roll_Move.cs
@@ -4,38 +4,80 @@ using UnityEngine;
 
 public class Roll_Move : MonoBehaviour
 {
-    private float _speed = 20f;
-
-    
+    public float _rotationSpeed = 360f; // 回転速度 (1秒で360度回転)
+    public int _numberRotations = 3; // 回転回数
+    private bool _isRotating = false; // 回転中かどうかを示すフラグ
+    private Collider objectCollider; // オブジェクトのコライダー
 
     // Start is called before the first frame update
     void Start()
     {
-
+        objectCollider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
-
-
-        if (Input.GetButtonDown("Lift_Roll"))
+        if(Input.GetButtonDown("Right_Roll") && !_isRotating)
         {
-
-            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
-            this.transform.Rotate(Vector3.left * _speed * Time.deltaTime);
-
+            StartCoroutine(RotateObject_Right());
         }
 
-        if (Input.GetButtonDown("Right_Roll"))
+        if (Input.GetButtonDown("Left_Roll") && !_isRotating)
         {
-
-            this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
-            this.transform.Rotate(Vector3.right * _speed * Time.deltaTime);
-
+            StartCoroutine(RotateObject_Left());
         }
 
+        //float _moveX = Input.GetAxis("Horizontal") * Time.deltaTime;
+        //float _moveY = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        //transform.Translate(new Vector3(_moveX, _moveY, 0));
+    }
+
+    private IEnumerator RotateObject_Right()
+    {
+        _isRotating = true;
+        objectCollider.enabled = false;
+
+        float totalRotation = 0f;
+        float targetRotation = 360f * _numberRotations;
+
+        while(totalRotation < targetRotation)
+        {
+            float rotationFrame = _rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward, rotationFrame);
+            totalRotation += rotationFrame;
+            yield return null;
+        }
+
+        transform.Rotate(Vector3.forward, targetRotation - totalRotation);
+
+        objectCollider.enabled = true;
+
+        _isRotating = false;
+    }
+
+    private IEnumerator RotateObject_Left()
+    {
+        _isRotating = true;
+        objectCollider.enabled = false;
+
+        float totalRotation = 0f;
+        float targetRotation = 360f * _numberRotations;
+
+        while (totalRotation < targetRotation)
+        {
+            float rotationFrame = _rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.back, rotationFrame);
+            totalRotation += rotationFrame;
+            yield return null;
+        }
+
+        transform.Rotate(Vector3.back, targetRotation - totalRotation);
+
+        objectCollider.enabled = true;
+
+        _isRotating = false;
     }
 }
