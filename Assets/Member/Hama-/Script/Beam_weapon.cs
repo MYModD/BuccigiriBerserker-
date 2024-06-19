73944179e85132d4401ec50772d46060a006214b
MorikuboP <orevshokuto10@gmail.com>
2024-06-19 15:01:55 +0900

    レーザーを作っているよ
    謝罪（阿部ちゃんに）

--- Assets/Member/Hama-/Script/Beam_weapon.cs
+++ Assets/Member/Hama-/Script/Beam_weapon.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Beam_weapon: MonoBehaviour
+public class Beam_weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ParticleSystem startWavePS;
+    public ParticleSystem startParticles;
+    public ParticleSystem smallMissiles;
+    public int smallMissilesCount = 100;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire3"))
+        {
+            startWavePS.Emit(1);
+            startParticles.Emit(smallMissilesCount);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var em = smallMissiles.emission;
+            em.enabled = true;
+        }
+        else
+        {
+            var em = smallMissiles.emission;
+            em.enabled = false;
+        }
     }
 }
+
