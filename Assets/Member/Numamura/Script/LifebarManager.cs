cb54713171d05ee61a386086cb0d87d5b5eecba9
numamura4416 <20235012-numamurarikuma@hcs.ac.jp>
2024-06-26 19:53:13 +0900

    LifeScript

--- Assets/Member/Numamura/Script/LifebarManager.cs
+++ Assets/Member/Numamura/Script/LifebarManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class LifebarManager : MonoBehaviour
 {
-    public GameObject[] lifeArray = new GameObject[3];
-    private int lifePoint = 6;
+    public GameObject[] lifeArray = new GameObject[6];
+    
+    private int lifePoint;
+    private int StartLife;
 
+
+    public PlayerLife playerLife;
+    private void Start()
+    {
+        StartLife = (int)playerLife.playerLife;
+        Debug.LogWarning(playerLife.playerLife);
+    }
     void Update()
     {
+        lifePoint = (int)playerLife.playerLife;
         if (Input.GetMouseButtonDown(0) && lifePoint < 6)
         {
             lifePoint++;
@@ -20,5 +30,21 @@ public class LifebarManager : MonoBehaviour
             lifeArray[lifePoint - 1].SetActive(false);
             lifePoint--;
         }
+
+        if (lifePoint < 6 && lifePoint >= 0)
+        {
+            lifeArray[lifePoint].SetActive(false);
+        }
+
+        if(lifePoint == 0)
+            
+        {
+            while(StartLife >= lifePoint)
+            {
+                lifeArray[lifePoint].SetActive(true);
+                lifePoint++;
+            }
+        }
+
     }
 }
