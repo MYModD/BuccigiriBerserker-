951835c5e4e8b2465984cd6a0c54438c13173746
Takemitio <haru10tkd@gmail.com>
2024-06-27 17:38:27 +0900

    リザルトと敵スポーンの改良

--- Assets/Member/Takemitio/Script/EnemySpawn.cs
+++ Assets/Member/Takemitio/Script/EnemySpawn.cs
@@ -8,6 +8,8 @@ public class EnemySpawn : MonoBehaviour
     [SerializeField] private float interval = 2.0f;       // 生成間隔（秒）
     [SerializeField] private float spawnDistance = 10.0f; // プレイヤーからの生成距離
     [SerializeField] private int maxSpawnCount = 3;       // 最大同時スポーン数
+    private int _groupEnemycnt;
+    [SerializeField] private int _totalEnemy;
 
     private Transform player;           // プレイヤーのTransform
 
@@ -25,19 +27,24 @@ public class EnemySpawn : MonoBehaviour
     {
         // 同時にスポーンするオブジェクト数をランダムで決定する
         int spawnCount = Random.Range(1, maxSpawnCount + 1);
-
-        for (int i = 0; i < spawnCount; i++)
+        if(_totalEnemy <= _groupEnemycnt)
         {
-            // プレイヤーの前方に一定の距離を保ってオブジェクトを生成する
-            Vector3 spawnPosition = player.position + -player.forward * spawnDistance;
+            for (int i = 0; i < spawnCount; i++)
+            {
+                // プレイヤーの前方に一定の距離を保ってオブジェクトを生成する
+                Vector3 spawnPosition = player.position + -player.forward * spawnDistance;
 
-            
-            // 生成位置にランダムなオフセットを加える（例として、-120～120のX軸方向、-80～80のY軸方向）
-            spawnPosition += new Vector3(Random.Range(-120f, 120f), Random.Range(-80f, 80f), 0f);
 
-            // 配列の中からランダムに選んでオブジェクトを生成する
-            int randomIndex = Random.Range(0, objectsToSpawn.Length);
-            Instantiate(objectsToSpawn[randomIndex], spawnPosition, transform.rotation);
+                // 生成位置にランダムなオフセットを加える（例として、-120～120のX軸方向、-80～80のY軸方向）
+                spawnPosition += new Vector3(Random.Range(-120f, 120f), Random.Range(-80f, 80f), 0f);
+
+                _groupEnemycnt += 5;
+                // 配列の中からランダムに選んでオブジェクトを生成する
+                int randomIndex = Random.Range(0, objectsToSpawn.Length);
+                Instantiate(objectsToSpawn[randomIndex], spawnPosition, transform.rotation);
+            }
+
         }
+
     }
 }
