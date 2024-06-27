95d41733f267df2e2f7a0ea7cad0955dca449215
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-06-27 17:49:12 +0900

    たけみちをのファイルをimportPackcagkeでついか♡

---
+++ Assets/Marge/Script/DestroyEnemyCount.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyEnemyCount : MonoBehaviour
+{
+    private TextMeshProUGUI textMeshPro;
+
+    public int value = 0;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            CountUp();
+        }
+    }
+
+    public void CountUp()
+    {
+        Debug.Log("‚æ‚Î‚ê‚½");
+        value++;
+        textMeshPro.text = value.ToString();
+
+
+    }
+
+}
