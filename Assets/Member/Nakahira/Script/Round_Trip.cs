71fbac70119eeeb7710b82aeb590e31b0036ff59
yuru58 <169129939+yuru58@users.noreply.github.com>
2024-06-28 12:34:15 +0900

    ラインレンダラー

---
+++ Assets/Member/Nakahira/Script/Round_Trip.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round_Trip : MonoBehaviour
+{
+    [SerializeField] private Transform pintA;
+    [SerializeField] private Transform pintB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
