cbfd7d8f1babf67af3b25be0c48d9162f94b2c34
numamura4416 <20235012-numamurarikuma@hcs.ac.jp>
2024-06-29 05:04:27 +0900

    ComboCounterAdd

--- Assets/Member/Numamura/Script/ComboCount.cs
+++ Assets/Member/Numamura/Script/ComboCount.cs
@@ -5,41 +5,35 @@ using UnityEngine.UI;
 
 public class ComboCount : MonoBehaviour
 {
-    public float totalTime = 180.0f; // タイマーの初期設定時間（秒）
-    private float timeLeft; // 現在の残り時間
+   
+    public Text ComboCountText; // UIテキストオブジェクト
+    public int ComboValue;
+    private bool _ComboCheck;
 
-    private float timemin;
-    private float timesec;
-    public Text countdownText; // UIテキストオブジェクト
 
+    public LifebarManager lifebarmanager;
     void Start()
     {
-        timeLeft = totalTime;
-        
-        countdownText = GetComponent<Text>();
+
+        ComboValue = 0;
+        ComboCountText = GetComponent<Text>();
         UpdateUI(); // UIを更新して初期設定時間を表示
     }
 
     void Update()
     {
-        // 残り時間を減らす
-        timeLeft -= Time.deltaTime;
-        timemin = timeLeft / 60;
-        timesec = timeLeft % 60;
-        Debug.Log(timemin);
-        Debug.Log(timesec);
+        _ComboCheck = lifebarmanager._ComboReset;
 
-        // タイマーが0以下になったら、指定した処理を実行する（ここでは例としてDebug.Log）
-        if (timeLeft <= 0.0f)
+        if(Input.GetKeyDown(KeyCode.Y))
         {
-            timeLeft = 0.0f;
-            timemin = 0.0f;
-            timesec = 0.0f;
-            Debug.Log("Time's up!");
-            // ここにゲームオーバー処理などを実装する
+            ComboValue++;
         }
 
-
+        if(_ComboCheck == true)
+        {
+            ComboValue = 0;
+            lifebarmanager._ComboReset = false;
+        }
 
         // UIを更新する
         UpdateUI();
@@ -48,17 +42,12 @@ public class ComboCount : MonoBehaviour
     void UpdateUI()
     {
         // UIテキストに残り時間を表示する
-        if (countdownText != null)
+        if (ComboCountText != null)
         {
-            if (timesec > 9)
-            {
-                //countdownText.text = Mathf.CeilToInt(timeLeft).ToString();
-                countdownText.text = Mathf.FloorToInt(timemin).ToString() + ":" + Mathf.FloorToInt(timesec).ToString();
-            }
-            if(timesec < 10)
-            {
-                countdownText.text = Mathf.FloorToInt(timemin).ToString() + ":" + "0" + Mathf.FloorToInt(timesec).ToString();
-            }
+            
+                ComboCountText.text = ComboValue.ToString() + "COMBO";
+            
+            
         }
     }
 }
