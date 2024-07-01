b845a1d64701a026c9d23d1042c1a3fb4d10b096
MorikuboP <orevshokuto10@gmail.com>
2024-07-01 13:32:54 +0900

    シーン遷移完成

--- Assets/Member/Hama-/Script/SceneLoader.cs
+++ Assets/Member/Hama-/Script/SceneLoader.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,38 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    private AsyncOperation sceneLoadOperation;
-    private bool sceneLoaded = false;
+  
 
-    void Start()
+    public void OnLoadSceneButtonClick()
     {
-        // 別のシーンを非同期でロードする
-        sceneLoadOperation = SceneManager.LoadSceneAsync("Enemy");
-        sceneLoadOperation.allowSceneActivation = false; // シーンのアクティベーションを待機状態にする
-        sceneLoadOperation.completed += OnSceneLoadComplete; // シーンのロード完了時のコールバックを設定
-    }
-    void Update()
-    {
-        Debug.Log("Loading progress: " + sceneLoadOperation.progress);
-    }
-
-    private void OnSceneLoadComplete(AsyncOperation asyncOperation)
-    {
-        sceneLoaded = true;
-         Debug.Log("Scene loaded successfully.");
-    }
-
-    public void LoadNextScene()
-    {
-        if (sceneLoaded)
-        {
-            // ロードしたシーンに遷移する
-            sceneLoadOperation.allowSceneActivation = true;
-        }
-        else
-        {
-            Debug.LogWarning("Scene is not yet loaded. Wait for the scene to load before attempting to transition.");
-            // ここで適切なエラー処理やメッセージを表示することができます
-        }
+            // 次のシーンへ遷移する
+            SceneManager.LoadScene("Enemy", LoadSceneMode.Single);
+      
     }
 }
